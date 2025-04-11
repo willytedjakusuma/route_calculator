f4fb4b6104df892fdc87f5c9a15aa9f805c16391
willytedjakusuma-moxi <willybudiman.tedjakusuma@moxiworks.com>
2025-04-11 13:29:52 +0800

    fix: parsing date in exchange rate from database.json

--- models/exchange_rate.rb
+++ models/exchange_rate.rb
@@ -7,9 +7,10 @@ class ExchangeRate < Record
   attr_accessor :date, :usd, :idr, :eur
 
   def initialize(data)
-    @date = Utils.format_string_to_date(data.first)
-    @usd = data.last["usd"]
-    @idr = data.last["idr"]
-    @eur = data.last["eur"]
+    date_key = data.keys.first
+    @date = Utils.format_string_to_date(data.keys.first)
+    @usd = data[date_key]["usd"]
+    @idr = data[date_key]["idr"]
+    @eur = data[date_key]["eur"]
   end
 end
